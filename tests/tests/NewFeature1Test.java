47f3c5fb8381bde9818c32e3cc5019df7bde0c02
Danila.Manturov <danila.manturov@jetbrains.com>
2021-11-19 16:25:24 +0300

    nf1 test

---
+++ tests/tests/NewFeature1Test.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NewFeature1Test {
+    @Test
+    public void test1() {
+        assertEquals(2, 2);
+    }
+}
